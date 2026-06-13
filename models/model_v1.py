0e82e302ba67f707c7f87b8db1d537755079b6a1
arolga-noo <arolga.noo@mail.ru>
2026-06-13 18:36:22 +0700

    Create model_v1.py

---
+++ models/model_v1.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+def train_and_save():
+    raw_csv_url = "https://githubusercontent.com"
+ 
+    df = pd.read_csv(raw_csv_url)
+    
+    X = df.drop(columns=['ID', 'default.payment.next.month'])
+    y = df['default.payment.next.month']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+       
+    all_features = X.columns.tolist()
+    preprocessor = ColumnTransformer(
+        transformers=[('scale', StandardScaler(), all_features)],
+        remainder='drop'
+    )
+    
+    pipeline = Pipeline([
+        ('preprocessor', preprocessor),
+        ('classifier', LogisticRegression(
+            C=1.0,
+            solver='lbfgs',
+            max_iter=1000,
+            random_state=42,
+            class_weight='balanced'
+        ))
+    ])
+    
+    pipeline.fit(X_train, y_train)
+
+    train_acc = pipeline.score(X_train, y_train)
+    print(f"Обучение завершено. Точность (Accuracy) на Train: {train_acc:.4f}")
+
+    os.makedirs('models', exist_ok=True)
+    model_output_path = 'models/model_v1.pkl'
+    
+    with open(model_output_path, 'wb') as f:
+        pickle.dump(pipeline, f)
+        
+    print(f"Модель сохранена в: {model_output_path}")
+
+if __name__ == "__main__":
+    train_and_save()
